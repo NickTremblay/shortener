eb3aec87867b89e5b378cd6cca3a10cd2816daca
NickTremblay <nickt1@uri.edu>
2024-01-24 15:38:13 -0500

    implement /shorten endpoint

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"errors"
 	"flag"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"time"
 
+	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go/v4"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 	
 
@@ -38,12 +44,14 @@ func main () {
 	////////////////
 	// init firebase
 	////////////////
+	// todo: iterate through configured list of expected collections and create them if they do not exist 
 	serviceAccountPathEnvVar, serviceAccountPathEnvVarExists := os.LookupEnv("SERVICE_ACCOUNT_PATH")
 	if(!serviceAccountPathEnvVarExists || serviceAccountPathEnvVar == "") { 
 		log.Fatal(errors.New("'SERVICE_ACCOUNT_PATH' environment variable undefined"))
 	}
 
 	opt := option.WithCredentialsFile(serviceAccountPathEnvVar)
+	// Firebase context 
 	ctx := context.Background()
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
@@ -65,8 +73,6 @@ func main () {
 	}
 	defer dbClient.Close()
 
-	
-
 	////////////
 	// init gin 
 	////////////
@@ -83,19 +89,57 @@ func main () {
 	// define routes
 	////////////////
 
-	// keep this route last or else users can override hard coded routes lol
+	router.POST("/shorten", func(c *gin.Context) {
+		// Todo: restrict this route to requests w auth token w verified uri email
+		// token := c.Request.Header["Token"]
+
+		// Todo: admin-configurable id length
+		newId, err := generateLinkId(4, &ctx, dbClient)
+		if err != nil { 
+			log.Printf("error generating link id: %s", err)
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		// Todo: sanitize and validate user input
+		url := ""
+		var json ShortenRequestBody
+        if err = c.BindJSON(&json); err != nil {
+            log.Printf("error binding ShortenRequestBody: %s", err)
+			c.String(http.StatusInternalServerError, err.Error())
+        }
+
+		url = json.Url 
+
+		link := Link {
+			Id: newId,                    
+			Url: url,                     
+			Author_Address: c.ClientIP(), 
+			Author_Email: "place@hold.er", // get from firebase auth
+		}
+
+		_, err = dbClient.Collection("links").Doc(newId).Set(ctx, link)
+        if err != nil {
+			log.Printf("error creating new document in collection 'links': %s", err.Error())
+			c.String(http.StatusInternalServerError, err.Error())
+        }
+
+		c.String(http.StatusOK, "http://" + c.Request.URL.Host + "/" + newId)
+	})
+
+	// keep this route last, or else shortened routes will override matching routes 
 	router.GET("/:linkId", func(c *gin.Context) {
         linkId := c.Param("linkId") 
         linkDoc, err := dbClient.Collection("links").Doc(linkId).Get(ctx)
 		if(err != nil) { 
 			// todo: 401 html 
+			c.String(http.StatusInternalServerError, err.Error())
 			log.Printf("error searching for link: %v\n", err)
 		} 
 
     	if(linkDoc.Data() != nil){ 
 			expandedUrl, err := linkDoc.DataAt("url")
 			if(err != nil) { 
-				c.String(http.StatusInternalServerError, "fuck")
+				c.String(http.StatusInternalServerError, err.Error())
 				log.Fatalf("error binding expandedUrl: %v\n", err)
 			}
 
@@ -118,3 +162,77 @@ func main () {
 		log.Fatal(err)
 	}
 }
+
+///////////////
+// define types 
+///////////////
+
+type Link struct {
+	Id                string    `json:"id" firestore:"id"`
+	Url               string    `json:"url" firestore:"url"`
+	Author_Address    string    `json:"author_address" firestore:"author_address"`
+	Author_Email      string    `json:"author_email" firestore:"author_email"`
+	Created           time.Time `json:"created" firestore:"created,serverTimestamp"`
+}
+
+type ShortenRequestBody struct { 
+	Url string `json:"url" binding:"required"`
+}
+
+///////////////////
+// define functions 
+///////////////////
+
+// Generate unique id for a Link. 
+func generateLinkId(n uint, ctx *context.Context, dbClient *firestore.Client) (string, error) { 	
+	newId, err := generateLinkToken(n)
+	if err != nil { 
+		return "", err
+	}
+
+	newIdUnique := false
+
+	linkDocRef := dbClient.Collection("links").Doc(newId)
+    _, err = linkDocRef.Get(*ctx)
+    if err != nil {
+        if status.Code(err) == codes.NotFound {
+            newIdUnique = true
+        } else {
+            return "", err
+        }
+    }
+
+	if newIdUnique { 
+		return newId, nil
+	} else { 
+		return generateLinkId(n, ctx, dbClient)
+	}
+}
+
+// Generate potentially non-unique id for a Link.
+func generateLinkToken(n uint) (string, error) { 
+	if(n == 0){ 
+		return "", nil
+	} else { 
+		symbols := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910"
+
+		// b is a random byte from the random reader
+		b := []byte{0}
+		if _, err := rand.Reader.Read(b); err != nil {
+			return "", err
+		}
+	
+		// random float on (0, 1]
+		x := float64(b[0]) / 255
+	
+		newSymbolIndex := int64(math.Floor(x * float64(len(symbols) - 1))) 
+		newSymbol := string(symbols[newSymbolIndex])
+
+		if rest, err := generateLinkToken(n - 1); err != nil { 
+			return "", err
+		}else { 
+			return newSymbol + rest, nil; 
+		}		
+	}
+}
+
